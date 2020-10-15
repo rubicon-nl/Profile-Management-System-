01216db730d069445d63828b825bab07650c9db3
Dieder <dieder.timmerman@gmail.com>
2020-10-15 15:25:22 +0200

    Add SteelToe package

--- ProfileService.Api/Startup.cs
+++ ProfileService.Api/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Steeltoe.Management.Endpoint;
+using Steeltoe.Management.Endpoint.Health;
 
 namespace ProfileService.Api
 {
@@ -35,6 +30,8 @@ namespace ProfileService.Api
                        Description = "Scan images services"
                    })
             );
+            services.AddHealthActuator(Configuration);
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +47,7 @@ namespace ProfileService.Api
             app.UseRouting();
 
             app.UseAuthorization();
+            
 
             app.UseEndpoints(endpoints =>
             {
